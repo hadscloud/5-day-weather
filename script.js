c4850eba736a4ec8a5c76534837fe48258097968
Hads <112281105+hadscloud@users.noreply.github.com>
2022-10-24 15:39:44 -0700

    adding html and begininng js

---
+++ script.js
@@ -0,0 +1,31 @@
+let weather = {
+    apiKey: "5cc7d3e4baaa36965b64ba5c4a4653fd",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "mph";
+        
+    }
+    
+};
